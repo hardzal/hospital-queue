bb561fbf2fea26dea96b5acdd9e69f5cfe30e366
hardzal <suryadijogja@gmail.com>
2021-12-21 15:47:34 +0700

    add another page

--- resources/views/home/histories.blade.php
+++ resources/views/home/histories.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app-home')
 
-@section('content')
+@section('header')
+<div class="col-sm-6">
+    <h1 class="m-0">History User</h1>
+</div><!-- /.col -->
+<div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">User</a></li>
+        <li class="breadcrumb-item active">Histories</li>
+    </ol>
+</div><!-- /.col -->
+@endsection
 
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                Riwayat User
+            </div>
+
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
